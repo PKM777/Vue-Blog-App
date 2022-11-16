483afa5bbee5ddc5d7d2149c3f62003360ab2ce0
Praveen K M <99317905+PKM777@users.noreply.github.com>
2022-11-17 04:11:02 +0530

    Vue-Blog-App initialization, setup and Top Navigation-bar creation

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <Head_file />
+ </div>
+</template>
+
+<script>
+import Head_file from './components/header-file.vue';
+export default{
+  name: 'App',
+  components:{
+    Head_file,
+  }
+
+  }
+
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  font-family: 'Kanit', sans-serif;
+}
+  
+</style>
---
+++ src/components/header-file.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+    
+        <ul class="nav_list">
+            <a href="#"><li class="list_items">Logo</li></a>
+            <a href="#"><li class="list_items">Home</li></a>
+            <a href="#"><li class="list_items">Catagories</li></a>
+            <a href="#"><li class="list_items">Trends</li></a>
+            <a href="#"><li class="list_items">Contact</li></a>
+        </ul>
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Head_file',
+        //dataline
+    data(){
+        return{
+            logoColor: 'red',
+        }
+    },
+    //method-line
+    methods:{
+        
+    }
+    }
+</script>
+
+<style>
+a{
+    text-decoration: none;
+}
+.nav_list{
+    display: flex;
+    list-style: none;
+    width: 100%;
+    height: 10vh;
+    margin: 0;
+    padding: 0;
+    align-items: center;
+    justify-content: space-around;
+    background-color: blueviolet;
+    
+    
+}
+.list_items{
+    font-weight: 600;
+    width: 10vw;
+    text-align: center;
+    color: white;
+}
+ul :first-child{
+    margin-right: auto;
+}
+</style>
